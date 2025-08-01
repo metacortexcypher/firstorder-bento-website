ebc14b6496f276fd5c4d75a4c7e1d61fc63a220a
metacortexcypher <prithvi.chigurupati76@gmail.com>
2025-08-02 02:07:36 +0530

    removed unused files

--- .next/static/chunks/codebase_firstorder bento_src_app_layout_tsx_5eefeaa6._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/codebase_firstorder bento_src_app_globals_4a0a8212.css",
-  "static/chunks/140f2_next_dist_971f08d1._.js",
-  "static/chunks/codebase_firstorder bento_src_components_ErrorReporter_tsx_f1d29155._.js"
-],
-    source: "dynamic"
-});
